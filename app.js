e531f9d34dd4b529198e51ad776ea16c5b731c02
Robert-Mal <33180@student.pwsztar.edu.pl>
2021-10-09 17:48:20 +0200

    user added

---
+++ app.js
@@ -0,0 +1,23 @@
+const _ = require('lodash');
+
+const user = {
+    name: 'Imie',
+    surname: 'Nazwisko',
+    allGrades: [
+        {
+            subjectName: 'Name1',
+            grades: [5,4,3,5,2],
+            weight: 3
+        },
+        {
+            subjectName: 'Name2',
+            grades: [3, 3.5, 2],
+            weight: 1
+        },
+        {
+            subjectName: 'Name3',
+            grades: [4, 3, 3],
+            weight: 5
+        }
+    ]
+}
